f23bce54f98e8c4b68e0172a4b3e071334061ae0
Dimitris Stefanakis <jimstef@outlook.com>
2022-01-23 23:15:08 +0200

    added react query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,23 @@ import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+ import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import Layout from "../src/flat/Layout";
 import { store } from "../src/store";
 import theme from "../src/theme";
 
+const queryClient = new QueryClient();
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ChakraProvider theme={theme}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </QueryClientProvider>
     </Provider>
   );
 }
